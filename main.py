9c0504bd3d481bb6f1434a8b36abb6bd9ea306e2
Nathaniel Masson <101712490+Naedrae-Razuul@users.noreply.github.com>
2023-04-16 08:13:27 -0400

    Update main.py
    I know, I know, it seems kind of pointless to update such a minuet amount of code, but I took a break from programming for a while, but I plan on getting back on it.
    Basically, I fixed inefficient/ineffective and redundant lines that slowed the program; even if it was just a little bit. I also made the code recyclable -- as much as I could.

--- main.py
+++ main.py
@@ -1,129 +1,118 @@
-import random;import time;import os
+# Made by Nathaniel Masson /// 10/26/22, updated 4/16/23
+#
+#
+import os
+import random
+import time
 
-# storing strings randomly chosen..
-bank = ['tiger', 'cat', 'dog', 'spider', 'insect', 'hello']
-# right here
-global x
-x = random.choice(bank)
-# counts characters in string chosen
-y = len(x)
+# stores random word chosen from 'bank.txt' to a variable.
+randint = random.randint(0, 213)
+f = open("./bank.txt", 'r')
+word = f.readlines()
+f.close()
+word = word[randint]
 
+# counts characters in the string chosen
+y = len(word)
 
+# exceptions
 special_chars = ['', ' ', ',', '<', '.', '>', '/', '?', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-remainder = "-"*y
+
+remainder = "-" * y
+
 # stores hangman graphics
+graphic = [("  -----\n"
+            "  |   |\n"
+            "      |\n"
+            "      |\n"
+            "      |\n"
+            "    -----"),
+           ("  -----\n"
+            "  |   |\n"
+            "  o   |\n"
+            "      |\n"
+            "      |\n"
+            "    -----"),
+           ("  -----\n"
+            "  |   |\n"
+            "  o   |\n"
+            "  |   |\n"
+            "      |\n"
+            "    -----"),
+           ("  -----\n"
+            "  |   |\n"
+            "  o / |\n"
+            "  |   |\n"
+            "      |\n"
+            "    -----"),
+           ("  -----\n"
+            "  |   |\n"
+            "\ o / |\n"
+            "  |   |\n"
+            "      |\n"
+            "    -----"),
+           ("  -----\n"
+            "  |   |\n"
+            "\ o / |\n"
+            "  |   |\n"
+            "      |\n"
+            "    -----"),
+           ("  -----\n"
+            "  |   |\n"
+            "\ o / |\n"
+            "  |   |\n"
+            "   \  |\n"
+            "    -----"),
+           ("  -----\n"
+            "  |   |\n"
+            "\ o / |\n"
+            "  |   |\n"
+            " / \  |\n"
+            "    -----")]
 
-graphic8 =    ("  -----\n"
-         "  |   |\n"
-         "      |\n"
-         "      |\n"
-         "      |\n"
-         "    -----")
-graphic7 =     ("  -----\n"
-          "  |   |\n"
-          "  o   |\n"
-          "      |\n"
-          "      |\n"
-          "    -----")
-graphic6 =    ("  -----\n"
-         "  |   |\n"
-         "  o   |\n"
-         "  |   |\n"
-         "      |\n"
-         "    -----")
-graphic5 =     ("  -----\n"
-          "  |   |\n"
-          "  o / |\n"
-          "  |   |\n"
-          "      |\n"
-          "    -----")
-graphic4 =    ("  -----\n"
-         "  |   |\n"
-         "\ o / |\n"
-         "  |   |\n"
-         "      |\n"
-         "    -----")
-graphic3 =    ("  -----\n"
-         "  |   |\n"
-         "\ o / |\n"
-         "  |   |\n"
-         "      |\n"
-         "    -----")
-graphic2 =      ("  -----\n"
-           "  |   |\n"
-           "\ o / |\n"
-           "  |   |\n"
-           "   \  |\n"
-           "    -----")
-graphic1 =     ("  -----\n"
-          "  |   |\n"
-          "\ o / |\n"
-          "  |   |\n"
-          " / \  |\n"
-          "    -----")
+points = 0
+while remainder != word:
+    print(graphic[points])
 
-
-# checks points everytime, and displays the man.. hanging..(?)
-def hangman():
-    if points == 8:
-        print(graphic8)
-    if points == 7:
-        print(graphic7)
-    if points == 6:
-        print(graphic6)
-    if points == 5:
-        print(graphic5)
-    if points == 4:
-        print(graphic4)
-    if points == 3:
-        print(graphic3)
-    if points == 2:
-        print(graphic2)
-    if points == 1:
-        print(graphic1)
-        print("\n\nYou lost!"); time.sleep(2)
-        exit
-
-
-points = 8
-while remainder != x:
-    hangman()
     print("\n" + remainder)
     guess = input("\n\nGuess a letter: ")
-
 
     cnt = len(guess)
 
     # check if input is greater than 1 character
     if cnt > 1:
-        print("\nMust use only one letter\n"); time.sleep(1)
+        print("\nMust use only one letter\n");
+        time.sleep(1)
         os.system('cls')
         continue
     # check if input has special characters
     if guess in special_chars:
-        print("\nMust only use letters\n"); time.sleep(1)
+        print("\nMust only use letters\n");
+        time.sleep(1)
         os.system('cls')
         continue
 
     # if correct..
-    if guess in x:
-        print('\ncorrect!\n'); time.sleep(1)
+    if guess in word:
+        print('\ncorrect!\n');
+        time.sleep(1)
         os.system('cls')
-        pos = x.rfind(guess)
-        remainder = remainder[:pos]+guess+remainder[pos+1:]
+        pos = word.rfind(guess)
+        remainder = remainder[:pos] + guess + remainder[pos + 1:]
 
     # if guess is.. guessed? incorrectly
     else:
-        print("\nGuess again! (incorrect)\n"); time.sleep(1)
-        points = points - 1
+        print("\nGuess again! (incorrect)\n");
+        time.sleep(1)
+        points = points + 1
         os.system('cls')
 
-    if points == 1:
-        print(graphic1)
+    if points == 8:
         print("\n\nYou lost!");
         time.sleep(2)
         exit
 
 # you won! Yay!
-print("The word was: " + x)
-print("\ncongrats! You won!!"); time.sleep(2)
+print("The word was: " + word)
+print("\ncongrats! You won!!");
+time.sleep(2)
